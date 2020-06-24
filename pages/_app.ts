5afeabf34d5031f188eeddd3b2e13de2dd6b6f83
Henry Hunt <henhen7.24@gmail.com>
2020-06-24 17:32:58 -0500

    moving to work on desktop

---
+++ pages/_app.ts
@@ -0,0 +1,13 @@
+import { ApolloProvider } from '@apollo/react-hooks';
+import { useApollo } from '../apollo/client';
+
+import '../css/index.scss'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  const myApolloClient = useApollo(pageProps.initialApolloState);
+
+  return (<(ApolloProvider: ApolloProvider) client = { myApolloClient } >
+    <Component { ...pageProps } />
+    </ApolloProvider>);
+}
