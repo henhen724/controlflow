edaeb7bfdf9c1795dc3b87c2675b0add431a69ce
henhen724 <hshunt@uchicago.edu>
2020-08-19 19:04:37 -0400

    Possible fix for the topology Issue

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { MongoError } from 'mongodb';
 mongoose.set('useCreateIndex', true);
 
 const connection = { isConnected: false };
@@ -6,12 +7,19 @@ const connection = { isConnected: false };
 
 const dbConnect = async () => {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(`${process.env.MONGODB_PROTO}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    });
+    const db = await mongoose.connect(`${process.env.MONGODB_PROTO}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        (err: MongoError) => {
+            if (err) {
+                console.error(err.message);
+            }
+        }
+    );
     console.log(`🗃️ Connected to the database at ${process.env.MONGODB_PROTO}${process.env.MONGODB_DOMAIN}`);
+    console.log(`Full URI: ${process.env.MONGODB_PROTO}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOMAIN}`)
     connection.isConnected = !!db.connections[0].readyState;
 }
 
